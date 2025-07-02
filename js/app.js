52666d03cf7f20b6bcef0db3c533038f581bbea3
alabbad74 <xsawbomb@gmail.com>
2025-07-02 12:54:26 +0300

    7-2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 /*-------------- Constants -------------*/
 
+const helpButton = document.getElementById('helpbutton');
+const playButton = document.getElementById('playbutton');
+const helpPopup = document.getElementById('helpPopup');
 
 /*---------- Variables (state) ---------*/
 
@@ -11,4 +14,16 @@
 
 
 /*----------- Event Listeners ----------*/
+// Play button for game.html
+playButton.addEventListener('click', function () {
+  window.location.href = 'game.html';
+});
 
+// Help button for the popup in same page
+helpButton.addEventListener('click', () => {
+  if (helpPopup.style.display === 'none') {
+    helpPopup.style.display = 'block';
+  } else {
+    helpPopup.style.display = 'none';
+  }
+});
